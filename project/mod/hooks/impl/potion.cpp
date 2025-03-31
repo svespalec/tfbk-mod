6888cd8738e5930b84b62dd23a5ff0945587bec4
sam <181611094+svespalec@users.noreply.github.com>
2025-03-30 22:37:09 -0700

    working potion mod

--- project/mod/hooks/impl/potion.cpp
+++ project/mod/hooks/impl/potion.cpp
@@ -2,7 +2,27 @@
 
 std::int64_t mod::hooks::handle_potion( void* this_ptr, std::int64_t potion_id )
 {
-  LOG( __FUNCTION__ );
+  auto potion_ptr = og::m_handle_potion( this_ptr, potion_id );
 
-  return og::m_handle_potion( this_ptr, potion_id );
+  if ( !potion_ptr )
+    return 0;
+
+  static int initial_potion_count = -1;
+
+  // get the current potion count pointer (recalculated each call)
+  auto potion_count_ptr = reinterpret_cast<int*>( potion_ptr + 0x20 );
+
+  // if this is the first call, save the initial count
+  if ( initial_potion_count == -1 )
+  {
+    initial_potion_count = *potion_count_ptr;
+    LOG("initial health potion count set to: {}", initial_potion_count );
+  }
+
+  // always restore to the initial amount
+  *potion_count_ptr = initial_potion_count;
+
+  LOG( "health potion count restored to: {}", initial_potion_count );
+
+  return potion_ptr;
 }
